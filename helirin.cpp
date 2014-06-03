b5f73977619779a1eca78c414a6da332c2838318
Alex Kerr <kirbyman62@gmail.com>
2014-06-03 21:07:37 +0100

    Got x and y mixed up, it seems to work now. I'm retarded

--- helirin.cpp
+++ helirin.cpp
@@ -44,18 +44,17 @@ void Helirin::move(Direction d, float frameTime)
 //Handles non-keyboard events, like rotation:
 void Helirin::handleEvents(float frameTime)
 {
-	//Checks if we recently collided, and if so,
-	//spin the other way around, and move in the
-	//opposite direction to the collision:
+	//Checks if we recently collided, and if so, spin the other way 
+	//around, and move in the opposite direction to the collision:
 	if(_hitTimer.getElapsedTime().asSeconds() <= COOLDOWN)
 	{
 		_sprite.rotate(-ROTATION_SPEED * frameTime);	
 		switch(_directionMoved)
 		{
-			case UP: 	_sprite.move(0, ((VELOCITY * 1.2) * frameTime)); break;
-			case DOWN:	_sprite.move(0, -((VELOCITY * 1.2) * frameTime));  break;
-			case LEFT:	_sprite.move(((VELOCITY * 1.2) * frameTime), 0); break;
-			case RIGHT:	_sprite.move(-((VELOCITY * 1.2) * frameTime), 0);  break;
+			case UP: 	_sprite.move(0, ((VELOCITY * 1.2) * frameTime));  break;
+			case DOWN:	_sprite.move(0, -((VELOCITY * 1.2) * frameTime)); break;
+			case LEFT:	_sprite.move(((VELOCITY * 1.2) * frameTime), 0);  break;
+			case RIGHT:	_sprite.move(-((VELOCITY * 1.2) * frameTime), 0); break;
 		}
 	}
 	else
@@ -64,27 +63,30 @@ void Helirin::handleEvents(float frameTime)
 
 bool Helirin::checkCollision(sf::ConvexShape& s)
 {
-	unsigned int nvert = s.getPointCount();
-
 	sf::FloatRect r = _sprite.getGlobalBounds();
 	sf::Vector2f p[4];
-	p[0] = sf::Vector2f(r.top, r.left);
-	p[1] = sf::Vector2f(r.top, (r.left + r.width));
-	p[2] = sf::Vector2f((r.top + r.height), r.left);
-	p[3] = sf::Vector2f((r.top + r.height), (r.left + r.width));
+	p[0] = sf::Vector2f(r.left, r.top);
+	p[1] = sf::Vector2f((r.left + r.width), r.top);
+	p[2] = sf::Vector2f(r.left, (r.top + r.height));
+	p[3] = sf::Vector2f((r.left + r.width), (r.top + r.height));
 
-	//Uses the ray casting algorithm on the 4 points of the Helirin, as found
+	unsigned int nvert = s.getPointCount();
+
+	//Uses the ray casting algorithm on the 4 corners of the Helirin, as found
 	//on www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html:
 	for(unsigned int i = 0; i < 4; i++)
 	{
+		bool collide = true;
 		for(unsigned int j = 0, k = (nvert - 1); j < nvert; k = j++)
 		{
 			if(((s.getPoint(j).y > p[i].y) != (s.getPoint(k).y > p[i].y)) &&
 				(p[i].x < (s.getPoint(k).x - s.getPoint(j).x) *
-				(p[j].y - s.getPoint(j).y) / 
+				(p[i].y - s.getPoint(j).y) / 
 				(s.getPoint(k).y - s.getPoint(j).y) + s.getPoint(j).x))
-			return true;	
+					collide = !collide;
 		}
+		if(collide)
+			return true;
 	}
 	return false;
 }
